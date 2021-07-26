a4a40ee904cd35f63a7eea9ee108bbc2ac267e00
Code Sandbox <hello@codesandbox.io>
2021-07-26 07:27:54 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.22c40bf9f6255c66de4b1901855ff2eb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f4b3b65b5e55a58f509346a597124e5",
+    "url": "https://code-sex.github.io/csb-oq66p/index.html"
+  },
+  {
+    "revision": "f8d4fcb4b86727535b9c",
+    "url": "https://code-sex.github.io/csb-oq66p/static/css/2.fbfbc609.chunk.css"
+  },
+  {
+    "revision": "43cfe5e96379a8c6ad8e",
+    "url": "https://code-sex.github.io/csb-oq66p/static/css/main.8647218e.chunk.css"
+  },
+  {
+    "revision": "f8d4fcb4b86727535b9c",
+    "url": "https://code-sex.github.io/csb-oq66p/static/js/2.543e3340.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "https://code-sex.github.io/csb-oq66p/static/js/2.543e3340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43cfe5e96379a8c6ad8e",
+    "url": "https://code-sex.github.io/csb-oq66p/static/js/main.5e3068c0.chunk.js"
+  },
+  {
+    "revision": "1d5236b6a23b348b3863",
+    "url": "https://code-sex.github.io/csb-oq66p/static/js/runtime-main.52a629ac.js"
+  },
+  {
+    "revision": "03282f245d88657216ad804049b99ed1",
+    "url": "https://code-sex.github.io/csb-oq66p/static/media/remixicon.03282f24.woff"
+  },
+  {
+    "revision": "3a4169303cb774dd27194bf6386eedd5",
+    "url": "https://code-sex.github.io/csb-oq66p/static/media/remixicon.3a416930.svg"
+  },
+  {
+    "revision": "7246a42055a8dec75a4e2385914cb657",
+    "url": "https://code-sex.github.io/csb-oq66p/static/media/remixicon.7246a420.eot"
+  },
+  {
+    "revision": "9479358960354d911407b4b8ebb7f8b2",
+    "url": "https://code-sex.github.io/csb-oq66p/static/media/remixicon.94793589.ttf"
+  },
+  {
+    "revision": "fae6476e288d27e075900554e01ddf3f",
+    "url": "https://code-sex.github.io/csb-oq66p/static/media/remixicon.fae6476e.woff2"
+  }
+]);
